7be236f042b7d216c24354519c468c091f216db4
mrjemson <andrew@jemson.org>
2021-07-04 18:48:20 +1000

    Added modifications for Linux to Windows and alt filepath syncing.

--- kodiplex/sync.py
+++ kodiplex/sync.py
@@ -21,13 +21,14 @@ class MediaSyncer:
     Note that if strict, checking is done before doing any updates.
     """
 
-    def __init__(self, a, b, mode: int, strict=False):
+    def __init__(self, a, b, mode: int, strict=False, logChanges=False, normalize={}):
         self.a = a
         self.b = b
         if not 0 <= mode <= 2:
             raise ValueError("mode must be 0,1 or 2")
         self.mode = mode
         self.strict = strict
+        self.normalize = normalize
 
     def verify(self):
         if not self.strict:
@@ -55,27 +56,49 @@ class MediaSyncer:
             [logger.error(str(x)) for x in inBnotA]
             raise Exception("Media mismatch!")
 
+    def normalizeNames(self, nPath):
+        if not self.normalize['enable']:
+            return nPath
+        else:
+            for pathMap in normalize['pathMap']:
+                nPath = nPath.replace(pathMap[0], pathMap[1])
+            nPath = nPath.replace('/', '\\') # For normalizing between Windows and Linux Paths
+            return nPath
+
     def unidirectionalSync(self):
         self.verify()
         for mediaA in self.a:
             for mediaB in self.b:
-                if mediaA == mediaB and mediaA.watched != mediaB.watched:
-                    mediaB.updateWatched(mediaA.watched)
+                if self.normalizeNames(mediaA.path) == self.normalizeNames(mediaB.path) and mediaA.watched != mediaB.watched:
+                    if self.logChanges:
+                        logger.info("Marking "+mediaB.path+" as watched.")
+                    try:
+                        mediaB.updateWatched(mediaA.watched)
+                    except:
+                        pass
 
     def bidirectionalSync(self):
         self.verify()
         for mediaA in self.a:
             for mediaB in self.b:
-                if mediaA == mediaB and mediaA.watched != mediaB.watched:
+                if self.normalizeNames(mediaA.path) == self.normalizeNames(mediaB.path) and mediaA.watched != mediaB.watched:
                     if self.mode == 1:
                         if not mediaA.watched:
+                            if self.logChanges:
+                                logger.info("Marking "+mediaA.path+" as watched.")
                             mediaA.updateWatched(True)
                         if not mediaB.watched:
+                            if self.logChanges:
+                                logger.info("Marking "+mediaB.path+" as watched.")
                             mediaB.updateWatched(True)
                     elif self.mode == 2:
                         if mediaA.watched:
+                            if self.logChanges:
+                                logger.info("Marking "+mediaA.path+" as unwatched.")
                             mediaA.updateWatched(False)
                         if mediaB.watched:
+                            if self.logChanges:
+                                logger.info("Marking "+mediaB.path+" as unwatched.")
                             mediaB.updateWatched(False)
 
     def sync(self):
@@ -118,7 +141,26 @@ def getFiles(thing):
 
 
 if __name__ == "__main__":
-    kodiMedia = getKodiMedia("http://localhost:8080")
-    plexMedia = getPlexMedia("http://192.168.0.100:32400")
-    sync = MediaSyncer(kodiMedia, plexMedia, 1, strict=False)
+    normalize = {}
+    normalize['pathMap'] = []
+
+    ### Begin Configuration Settings ###
+    normalize['enable'] = True
+    normalize['pathMap'].append(['smb://10.1.1.10/Movies', 'M:'])
+    normalize['pathMap'].append(['smb://10.1.1.10/TV Shows', 'T:'])
+
+    logChanges = True
+
+    strict = False
+
+    syncDir = 0 ### 0/1/2 - See README
+    
+    kodiURL = 'http://10.1.1.1:80'
+    plexURL = 'http://10.1.1.2:32400'
+    plexToken = 'a1b2c3d4e5'
+    ### End Configuration Settings ###
+
+    kodiMedia = getKodiMedia(kodiURL)
+    plexMedia = getPlexMedia(plexURL, plexToken)
+    sync = MediaSyncer(kodiMedia, plexMedia, syncDir, strict, logChanges, normalize)
     sync.sync()
